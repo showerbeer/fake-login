9eb6b45f2185713788a4cc5654bae4c9c85bb706
Christian Lassen <Christian.Lassen@health.nsw.gov.au>
2020-09-13 21:17:48 +1000

    Allows CORS

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const morgan = require('morgan');
+const cors = require('cors');
 const attempts = require('./attempts');
 
 module.exports.init = function (configs, db) {
     const app = express();
+    app.use(cors({origin: /.*.lassen.dev.*/, methods: ['GET', 'POST']}))
     app.use(compression());
     app.use(morgan(configs.logger.format));
     app.use(bodyParser.json());
